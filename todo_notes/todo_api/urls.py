ff009b7851e776140359d5a26ab725558bc9a0ab
vasskess <vrangelov85@abv.bg>
2024-04-01 20:11:21 +0300

    blacked project

--- todo_notes/todo_api/urls.py
+++ todo_notes/todo_api/urls.py
@@ -3,5 +3,5 @@ from todo_api import views
 
 urlpatterns = [
     path("notes/", views.TodoNoteListCreate.as_view(), name="note-list"),
-    path("notes/delete/<int:pk>/", views.TodoNoteDelete.as_view(), name="note-delete")
+    path("notes/delete/<int:pk>/", views.TodoNoteDelete.as_view(), name="note-delete"),
 ]
