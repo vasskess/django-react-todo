ec34110e4a8b13433efb3ff9be0de833db12712f
vasskess <vrangelov85@abv.bg>
2024-04-27 20:31:59 +0300

    add fron-end functionality

---
+++ todo_notes/frontend/src/App.jsx
@@ -0,0 +1,41 @@
+import react from "react"
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Home from './pages/Home'
+import NotFound from './pages/NotFound'
+import ProtectedRout from './components/ProtectedRout'
+
+
+function Logout() {
+  localStorage.clear()
+  return <Navigate to='/login/' />
+}
+
+function RegisterAndLogout() {
+  localStorage.clear()
+  return <Register />
+}
+
+function App() {
+  return (
+  <BrowserRouter>
+    <Routes>
+      <Route
+        path='/'
+        element={
+          <ProtectedRout>
+            <Home />
+          </ProtectedRout>
+      }
+      />
+      <Route path='/login' element={<Login />}/>
+      <Route path='/logout' element={<Logout />}/>
+      <Route path='/register' element={<RegisterAndLogout />}/>
+      <Route path='*' element={<NotFound />}/>
+    </Routes>
+  </BrowserRouter>
+  )
+}
+
+export default App
